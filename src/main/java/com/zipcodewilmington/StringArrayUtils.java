10bcbbcb550de1fc04c895887d4afa6d3a7db57f
Kievina <56592501+Kievina@users.noreply.github.com>
2019-10-23 18:45:52 -0400

    all except last 2 methods completed

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -7,7 +9,7 @@ public class StringArrayUtils {
     /**
      * @param array array of String objects
      * @return first element of specified array
-     */ // TODO
+     */
     public static String getFirstElement(String[] array) {
         return array[0];
     }
@@ -25,40 +27,54 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
     }
 
     /**
      * @param array array of String objects
      * @return second to last element in specified array
-     */ // TODO
+     */
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
      * @param array array of String objects
      * @param value value to check array for
      * @return true if the array contains the specified `value`
-     */ // TODO
+     */
     public static boolean contains(String[] array, String value) {
-        return false;
+        boolean containsValue = false;
+        for (String s : array) {
+            if (s.contains(value)) {
+                containsValue = true;
+            }
+        }
+        return containsValue;
     }
 
     /**
      * @param array of String objects
      * @return an array with identical contents in reverse order
-     */ // TODO
+     */
     public static String[] reverse(String[] array) {
-        return null;
+        ArrayList<String> strArrayList = new ArrayList<String>(Arrays.asList(array));
+        Collections.reverse(strArrayList);
+        String[] newReversedArray = strArrayList.toArray(new String[strArrayList.size()]);
+        return newReversedArray;
     }
 
     /**
      * @param array array of String objects
      * @return true if the order of the array is the same backwards and forwards
-     */ // TODO
+     */
     public static boolean isPalindromic(String[] array) {
-        return false;
+        String[] reversedArray = reverse(array);
+        if (Arrays.equals(array, reversedArray)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -66,7 +82,22 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        // create a String array with letters from alphabet
+        String[] alphabet = {"a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                "u", "v", "w", "x", "y", "z"};
+        // combine elements in array into one string
+        String arrayJoin = String.join("", array).toLowerCase();
+        int countLetters = 0;
+        //write for loop to iterate through string array of alphabet letters
+        for (int i = 0; i < alphabet.length; i++) {
+            // check if element of alphabet array is contained in array
+            if (arrayJoin.contains(alphabet[i])) {
+                countLetters = countLetters + 1;
+            }
+        }
+        return countLetters == 26;
+
     }
 
     /**
@@ -75,16 +106,26 @@ public class StringArrayUtils {
      * @return number of occurrences the specified `value` has occurred
      */ // TODO
     public static int getNumberOfOccurrences(String[] array, String value) {
-        return 0;
+        int numOfOccurrences = 0;
+        ArrayList<String> newArrayList = new ArrayList<String>(Arrays.asList(array));
+        for (String strArr : newArrayList) {
+            if (strArr.equals(value)) {
+                numOfOccurrences = numOfOccurrences + 1;
+            }
+        }
+        return numOfOccurrences;
     }
 
     /**
      * @param array         array of String objects
      * @param valueToRemove value to remove from array
      * @return array with identical contents excluding values of `value`
-     */ // TODO
+     */
     public static String[] removeValue(String[] array, String valueToRemove) {
-        return null;
+        ArrayList<String> newArrayList = new ArrayList<String>(Arrays.asList(array));
+        newArrayList.remove(valueToRemove);
+        String[] valueRemovedArray = newArrayList.toArray(new String[newArrayList.size()]);
+        return valueRemovedArray;
     }
 
     /**
